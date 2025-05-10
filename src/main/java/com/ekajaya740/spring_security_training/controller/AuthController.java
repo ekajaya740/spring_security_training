4d5801a76d849619183c7d39b4b7d0d1c22713ca
ekajaya <ekajaya740@gmail.com>
2025-05-10 17:01:56 +0800

    fix: add token

--- src/main/java/com/ekajaya740/spring_security_training/controller/AuthController.java
+++ src/main/java/com/ekajaya740/spring_security_training/controller/AuthController.java
@@ -4,7 +4,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ekajaya740.spring_security_training.service.TokenService;
@@ -24,7 +23,11 @@ public class AuthController {
   public String token(Authentication authentication) {
     log.debug("Token requested for user: {}", authentication.getName());
 
-    return '';
+    String token = tokenService.generateToken(authentication);
+
+    log.debug("Token granted: {}", token);
+
+    return token;
   }
 
 }
